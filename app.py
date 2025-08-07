f1c8742dba87a371295b61d20c139ca114d03b99
MohammadSharifurRahman <imsohel@gmail.com>
2025-08-07 20:09:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,7 @@ def main():
         log_level="info" if not debug else "debug"
     )
 
+
 if __name__ == "__main__":
-    main()
+    app = create_app()
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8001)))
